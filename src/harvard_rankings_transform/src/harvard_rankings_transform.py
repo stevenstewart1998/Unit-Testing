16d71b1fae18e5313669e47b5e1b305fa296f3b8
Steven Stewart <steven.stewart3@accesscfa.com>
2024-04-11 03:37:53 -0400

    push completed examples

--- src/harvard_rankings_transform/src/harvard_rankings_transform.py
+++ src/harvard_rankings_transform/src/harvard_rankings_transform.py
@@ -3,54 +3,106 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 import pyspark 
-from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.functions import col
-import pandas
+from pyspark.sql.window import Window 
+
+from pyspark.sql import SparkSession, DataFrame, Column
+from pyspark.sql.functions import (col, lit, max, min, substring, try_divide, coalesce, lead, lag, avg, round)
+from pyspark.sql.types import IntegerType, FloatType
+
 from kaggle.api.kaggle_api_extended import KaggleApi
 from zipfile import ZipFile
-
+from typing import Optional, List
 
 dataset_name = "sahirmaharajj/air-pollution-dataset"
 file_name = "Air_Quality.csv"
 data_path = "src/harvard_rankings_transform/data"
-spark = SparkSession.builder.getOrCreate()
+spark: SparkSession = SparkSession.builder.getOrCreate()
 
 def instantiate_kaggle_api():
     api = KaggleApi()
     api.authenticate()
     return api
 
-def unzip_file(file_name, path):
+def unzip_file(file_name, path) -> bool:
     zipped_file_name =  f"{path}/{file_name}.zip"
-    with ZipFile(zipped_file_name, "r") as unzipped_file:
-        unzipped_file.extractall(path)
+    try:
+        with ZipFile(zipped_file_name, "r") as unzipped_file:
+            unzipped_file.extractall(path)
+        return True
+    except Exception as e:
+        print(e)
+        return False
     
 def read_dataset(spark: SparkSession, 
                  api: KaggleApi, 
                  dataset_name: str, 
                  file_name: str, 
-                 path:str) -> DataFrame:
-    dataset = api.dataset_download_file(dataset_name, file_name, path, True)
-    print(dataset)
+                 path:str) -> Optional[DataFrame]:
+    try:
+        api.dataset_download_file(dataset_name, file_name, path, True)
+    except Exception as e: 
+        print(e)
+        return None
     unzip_file(file_name, path)
     unzipped_file_path = f"{path}/{file_name}"
     df = spark.read.csv(path=unzipped_file_path, header=True)
     return df
-# metrics
-    # distinct measure name
-    # 
 
 # BEGIN FUNC
-def get_distinct_name(df: DataFrame) -> DataFrame:
-    pass
 
+def get_columns(df: DataFrame, metrics: List[str] = []) -> DataFrame:
+    return (
+        df.select(
+            *metrics, 
+            col("Start_Date"),
+            col("Data Value").alias("value")
+        )
+    )
+
+
+def parse_year(col: Column) -> Column:
+    parsed_column = substring(col, -4, 4).cast(IntegerType())
+    return parsed_column
+
+def get_annual_percent_change(max_value: Column, min_value: Column, max_year: Column = lit(1), min_year: Column = lit(1)) -> DataFrame:
+    time_period = coalesce(try_divide(lit(1),(max_year - min_year)), lit(1))
+    total_change = (max_value - min_value).cast(FloatType())
+    percentage_change = coalesce(try_divide(total_change, min_value), max_value)
+    annual_percent_change = round(percentage_change * time_period, 4)
+    return annual_percent_change * 100
+
+def agg_by_metric(df: DataFrame, metrics: List[col]) -> DataFrame:
+    return (
+        df.groupBy(metrics).agg(
+            round(get_annual_percent_change(
+            max("value").alias("max_value"),
+            min("value").alias("min_value"),
+            max("year").alias("max_year"),
+            min("year").alias("min_year")
+            ), 4).alias("total_percent_change")          
+            )
+        )
 
 # END FUNC
+
+def write_to_csv(df: DataFrame):
+    df.show(100, truncate=False)
+
 
 api = instantiate_kaggle_api()
 df = read_dataset(spark, api, dataset_name, file_name, data_path)
 
+if not df:
+    raise Exception("Dataset failed to be returned")
 
 
-    
+metric_list = ["Indicator ID", "Name", "Geo Join ID", "Geo Place Name"]
+
+df_metrics = get_columns(df, metric_list) 
+df_metrics = df_metrics.withColumn("year", parse_year(col("Start_Date")))
+df_metrics = df_metrics.drop("Start_Date")
+df_agg = agg_by_metric(df_metrics, metric_list)
+
+
+write_to_csv(df_agg)
 
--- src/harvard_rankings_transform/tests/test_harvard_rankings_transform.py
+++ src/harvard_rankings_transform/tests/test_harvard_rankings_transform.py
@@ -3,12 +3,23 @@ import re
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 from copy import deepcopy
-from pyspark.sql import SparkSession, DataFrame
-
+from pyspark.sql import SparkSession, DataFrame, Column
+from pyspark.testing.utils import assertDataFrameEqual
+from typing import Optional, List
+from pyspark.sql.functions import (col, lit, max, min, substring, try_divide, coalesce, lead, lag, avg, round)
+import pandas as pd
+from pyspark.sql.types import IntegerType, FloatType
+import os
+import sys
 
 @pytest.fixture(name="spark")
 def spark_fixture():
-    spark = SparkSession.builder.appName("Testing PySpark Example").getOrCreate()
+    
+    os.environ['PYSPARK_PYTHON'] = sys.executable
+    os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+    spark:SparkSession = SparkSession.Builder().master("local[1]").getOrCreate()
+    spark.sparkContext.setLogLevel('WARN')
+
     yield spark
 
 @pytest.fixture
@@ -21,6 +32,63 @@ def modules_list():
     local_clone = deepcopy(locals())
     return local_clone
 
-def test_modules(spark, modules_list):
-    module = modules_list.get("get_distinct_name")
-    raise Exception(module)
+
+def test_get_columns(spark: SparkSession, modules_list):
+    get_columns = modules_list.get("get_columns")
+
+    df_input = spark.createDataFrame(
+        data=[["val1",  "val2", "12/01/2021",  "val1",   "1",  "2"]],
+        schema=["col1", "col2", "Start_Date", "Data Value", "extra1", "extra2"]
+    )
+    df_expected = spark.createDataFrame(
+        data=[["val1", "val2", "12/01/2021","val1"]],
+                schema=["col1", "col2", "Start_Date", "value"]
+    )
+    metrics = ("col1", "col2")
+    df_actual: DataFrame = get_columns(df_input, metrics)
+    # assert df_actual.collect() == df_expected.collect()
+
+    assertDataFrameEqual(df_actual, df_expected)
+
+def test_parse_year(spark, modules_list):
+    parse_year = modules_list.get("parse_year")
+    df_input = spark.createDataFrame(
+        data=[["12/27/2018"]],
+        schema=["col1"]
+    )
+    df_expected = spark.createDataFrame(
+        data=[["12/27/2018", 2018]],
+                schema=["col1", "year"]
+    ).withColumn("year", col("year").cast(IntegerType()))
+
+    df_actual = df_input.withColumn(
+        "year", parse_year("col1")
+    )
+    assertDataFrameEqual(df_actual, df_expected)
+
+@pytest.mark.parametrize(
+    "max,min,max_year,min_year,result",
+    [
+        (100, 50, 2022, 2021, 100.0),
+        (75, 50, None, None, 50.0)
+    ]
+)
+def test_get_annual_percent_change(spark: SparkSession, modules_list,
+                                   max, min, max_year, min_year, result):
+    get_annual_percent_change = modules_list.get("get_annual_percent_change")
+
+    data = [["val1"]]
+    df_input = spark.createDataFrame(data=data, schema=["col1"])
+
+    if max_year is None or min_year is None:
+        df_actual = df_input.select(get_annual_percent_change(lit(max), lit(min)).alias("result"))
+    else:
+        df_actual = df_input.select(get_annual_percent_change(lit(max), lit(min), lit(max_year), lit(min_year)).alias("result"))
+    df_expected = spark.createDataFrame(data= [[result]], schema=["result"])
+
+    assertDataFrameEqual(df_actual, df_expected)
+
+
+
+
+
